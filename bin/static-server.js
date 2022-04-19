9f1bd229e0c3c918e214f1d21bfa116ce91cd46c
陶雅阁 <taoyage@taoyagedeMacBook-Pro.local>
2022-04-19 21:56:01 +0800

    chores: update package

--- bin/static-server.js
+++ bin/static-server.js
@@ -7,13 +7,22 @@ const StaticServer = require('../src/app');
 const pkg = require(path.join(__dirname, '..', 'package.json'));
 
 const options = yargs
-  .version(pkg.name + '@' + pkg.version)
+  .version('v', '显示版本号', pkg.name + '@' + pkg.version)
   .usage('yg-server [options]')
-  .option('p', { alias: 'port', describe: '设置服务器端口号', type: 'number', default: config.port })
-  .option('o', { alias: 'openbrowser', describe: '是否打开浏览器', type: 'boolean', default: config.openbrowser })
+  .option('p', {
+    alias: 'port',
+    describe: '设置服务器端口号',
+    type: 'number',
+    default: config.port,
+  })
+  .option('o', {
+    alias: 'openbrowser',
+    describe: '是否打开浏览器',
+    type: 'boolean',
+    default: config.openbrowser,
+  })
   .option('n', { alias: 'host', describe: '设置主机名', type: 'string', default: config.host })
   .option('c', { alias: 'cors', describe: '是否允许跨域', type: 'string', default: config.cors })
-  .option('v', { alias: 'version', type: 'string' })
   .example('yg-server -p 8000 -o localhost', '在根目录开启监听8000端口的静态服务器')
   .help('h').argv;
 
